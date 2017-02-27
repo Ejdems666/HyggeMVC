358040703bdf27bd856323d61c5620b78d2e5622
Adam Becvar <becvar.adam666@gmail.com>
2017-02-27 14:09:16 +0100

    remove project specific constants from Constructor

--- src/main/java/hyggemvc/controller/Controller.java
+++ src/main/java/hyggemvc/controller/Controller.java
@@ -2,28 +2,41 @@ package hyggemvc.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by adam on 25/02/2017.
  */
 public abstract class Controller {
+    private boolean wasRedirected = false;
+
     protected final HttpServletRequest request;
     protected final HttpServletResponse response;
 
-    public Controller(HttpServletRequest request,HttpServletResponse response) {
+    public Controller(HttpServletRequest request, HttpServletResponse response) {
         this.request = request;
         this.response = response;
     }
 
-    protected void renderTemplate(String template){
+    protected void setTemplateOutput(String template) {
         response.setContentType("text/html");
-        request.setAttribute("template",template);
+        request.setAttribute("template", template);
 
-        request.setAttribute("root","/");
-        request.setAttribute("assets","/assets/");
-
-        if(request.getAttribute("title") == null) {
-            request.setAttribute("title",template);
+        if (request.getAttribute("title") == null) {
+            request.setAttribute("title", template);
         }
+    }
+
+    protected void redirect(String url) {
+        try {
+            response.sendRedirect(url);
+            wasRedirected = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean wasRedirected() {
+        return wasRedirected;
     }
 }
