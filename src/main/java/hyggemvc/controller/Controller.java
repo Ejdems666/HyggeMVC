7f0fa0098cd0bde98d1d88956041262e9e193bc7
Adam Becvar <becvar.adam666@gmail.com>
2017-05-16 14:49:12 +0200

    added posibility to specify layout other than just index.jsp

--- src/main/java/hyggemvc/controller/Controller.java
+++ src/main/java/hyggemvc/controller/Controller.java
@@ -28,6 +28,10 @@ public abstract class Controller {
     }
 
     protected void renderTemplate(String template) {
+        renderTemplate(template,"index");
+    }
+
+    protected void renderTemplate(String template, String layout) {
         response.setContentType("text/html");
         request.setAttribute("template", template);
         request.setAttribute("alerts", getAlerts());
@@ -36,7 +40,7 @@ public abstract class Controller {
             request.setAttribute("title", template);
         }
         try {
-            request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+            request.getRequestDispatcher("/WEB-INF/"+layout+".jsp").forward(request, response);
         } catch (ServletException | IOException e) {
             e.printStackTrace();
         }
